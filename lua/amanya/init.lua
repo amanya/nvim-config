27d2f15f044b875673a43a715342133421d099fb
Albert Manya <albert@t460s.local>
2024-04-12 23:39:27 +0200

    Basic neovim config

---
+++ lua/amanya/init.lua
@@ -0,0 +1,9 @@
+require("amanya.set")
+require("amanya.remap")
+require("amanya.lazy_init")
+
+vim.cmd.colorscheme("tokyonight")
+
+vim.g.netrw_browse_split = 0
+vim.g.netrw_banner = 0
+vim.g.netrw_winsize = 25
