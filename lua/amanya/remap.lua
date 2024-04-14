7d643e47bbc0b325457a8471cc2b28af12613de0
Albert Manyà <albert.manya@adevinta.com>
2024-04-14 18:07:41 +0200

    maps to ease moving between splits

--- lua/amanya/remap.lua
+++ lua/amanya/remap.lua
@@ -3,3 +3,8 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("n", "<leader><leader>", function()
     vim.cmd("so")
 end)
+
+vim.keymap.set("n", "<c-j>", "<c-w>j")
+vim.keymap.set("n", "<c-h>", "<c-w>h")
+vim.keymap.set("n", "<c-k>", "<c-w>k")
+vim.keymap.set("n", "<c-l>", "<c-w>l")
